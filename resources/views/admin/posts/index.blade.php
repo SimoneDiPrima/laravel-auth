6a2a2de918674fac1ca46f3cfb9d30dfa893f87a
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-09-27 18:03:05 +0200

    creazione index.blade

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,5 +3,33 @@
 
 <header class="container">
     <h1>LA LISTA DEI MIEI POST</h1>
+    <table class="table table-dark table-striped">
+        <thead>
+        <tr>
+            <td>#</td>
+            <td>Titolo</td>
+            <td>Slug</td>
+            td
+            <td>Creato</td>
+            <td>Modificato</td>
+        </tr>
+        </thead>
+        <tbody>
+            @forelse($posts as $post)
+            <tr>
+                <th scope="row">{{$post->id}}</th>
+                <td>{{$post->title}}</td>
+                <td>{{$post->slug}}</td>
+                <td>{{$post->content}}</td>
+                <td>{{$post->created_at}}</td>
+                <td>{{$post->update_at}}</td>
+            </tr>
+            @empty
+            <tr>
+                <td colspan="6">NON SONO PRESENTI POST</td>
+            </tr>
+            @endforelse
+        </tbody>
+    </table>
 </header>
 @endsection
