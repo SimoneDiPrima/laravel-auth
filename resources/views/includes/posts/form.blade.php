c320b587b234ac215008fb08c37c07ed570f2419
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-09-28 14:48:27 +0200

    creazione model e migration category/categories

--- resources/views/includes/posts/form.blade.php
+++ resources/views/includes/posts/form.blade.php
@@ -11,7 +11,7 @@
   <div class="form-group">
     <label for="title">TITOLO</label>
     <input type="text" class="form-control" id="title" name="title" value="{{ old('title', $post->title ) }}" aria-describedby="emailHelp">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    <small class="form-text text-muted">We'll never share your Privacy with anyone else.</small>
   </div>
  
   <div class="form-group">
