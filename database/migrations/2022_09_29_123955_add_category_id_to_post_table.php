e227cde5d1987d347ee646bbe7f519c367644480
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-09-30 15:49:56 +0200

    add category id on post table

--- database/migrations/2022_09_29_123955_add_category_id_to_post_table.php
+++ database/migrations/2022_09_29_123955_add_category_id_to_post_table.php
@@ -13,7 +13,10 @@ class AddCategoryIdToPostTable extends Migration
      */
     public function up()
     {
-        
+        Schema::table('posts', function (Blueprint $table) {
+            $table->unsignedBigInteger('category_id')->nullable()->after('id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
+        });
     }
 
     /**
@@ -24,7 +27,9 @@ class AddCategoryIdToPostTable extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->dropForeign('posts_category_id_foreign');
+
+            $table->dropColumn('category_id');
         });
     }
 }
