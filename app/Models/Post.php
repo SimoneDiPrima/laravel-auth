819b3bd8a57d6dda02834ec47af76f461d22c45a
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-10-03 16:17:03 +0200

    relazione Post/Tag e creazione tabella pivot

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -15,4 +15,8 @@ class Post extends Model
     public function author(){
         return $this->belongsTo('App\User','user_id','id');
     }
+
+    public function tags(){
+        return $this->belongsToMany('App\Models\Tag');
+    }
 }
