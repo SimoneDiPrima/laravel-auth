509ce2f26453031294260350b7fdfdc07408084f
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-10-03 16:08:56 +0200

    migrate tags_table e creazione TagSeeder

--- database/migrations/2022_10_03_134603_create_tags_table.php
+++ database/migrations/2022_10_03_134603_create_tags_table.php
@@ -16,6 +16,7 @@ class CreateTagsTable extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->string('label')->unique();
+            $table->char('color',7)->unique();
             $table->timestamps();
         });
     }
--- database/seeds/TagSeeder.php
+++ database/seeds/TagSeeder.php
@@ -1,6 +1,7 @@
 <?php
-
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TagSeeder extends Seeder
 {
@@ -9,8 +10,16 @@ class TagSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run( Faker $faker )
     {
-        //
+        $labels =['Frontend','Backend','UX/UI','FullStack','DevOps'];
+        foreach($labels as $label){
+
+               $tag = new Tag();
+               $tag->label = $label;
+               $tag->color = $faker->hexColor();
+               $tag->save();
+
+        }
     }
 }
